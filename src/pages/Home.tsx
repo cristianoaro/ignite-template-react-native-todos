55dd335142ddbcff8becf2b1d81cf89bddf6a8b8
Cristiano Aro de Oliveira <cristiano_sx@hotmail.com>
2022-01-17 16:08:01 -0300

    Solving Challeng - All Tests 100%

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,39 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    let id = new Date().getTime()
+    let task = {
+      id,
+      title: newTaskTitle,
+      done: false,
+    }
+    setTasks(oldTasks => [...oldTasks, task])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    setTasks(oldState => oldState.filter(
+      task => {
+        if (task.id === id) {
+          if (task.done) {
+            task.done = false
+            return task
+          }
+          else {
+            task.done = true
+            return task
+          }
+        }
+        else {
+          return task
+        }
+      }
+    ))
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldState => oldState.filter(
+      task => task.id !== id
+    ))
   }
 
   return (
@@ -26,10 +50,10 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
   )
